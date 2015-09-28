9fdb8e063b7e3d57b7e0dc68ae16f38c013b36e2
vietkungfu <vietbktn@gmail.com>
2015-09-28 09:20:05 +0900

    Initial Commit

---
+++ DiaryNote/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DiaryNote
+//
+//  Created by IC経営戦略室App開発用 on 2015/09/28.
+//  Copyright (c) 2015年 ic-net. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DiaryNote/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  DiaryNote
+//
+//  Created by IC経営戦略室App開発用 on 2015/09/28.
+//  Copyright (c) 2015年 ic-net. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
